a528481abd06a2a39f6783fbdbb40860b5a8954d
Kauedq22 <kauedq12@gmail.com>
2023-06-02 20:11:06 -0300

    BoardException

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,16 +1,18 @@
 package boardgame;
 
 public class Position {
-    private int column;
     private int row;
+    private int column;
+    
 
     public Position(){
 
     }
 
-    public Position(int column, int row) {
-        this.column = column;
+    public Position( int row, int column) {
+        
         this.row = row;
+        this.column = column;
     }
 
     public int getColumn() {
