c4ddd5c8c5df10f748508f80a269e4c2e57c5357
Kauedq22 <kauedq12@gmail.com>
2023-06-04 12:30:57 -0300

    Moving piece

---
+++ bin/src/boardgame/BoardExeption.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoardExeption extends RuntimeException{
+    private static final long serialVersionUID =1l;
+    
+    public BoardExeption(String msg){
+        super(msg);
+    }
+}
---
+++ src/chess/ChessException.java
@@ -0,0 +1,9 @@
+package chess;
+
+import boardgame.BoardExeption;
+
+public class ChessException extends BoardExeption  {
+    public ChessException(String msg){
+        super(msg);
+    }
+}
